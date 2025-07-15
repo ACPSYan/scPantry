41f732b8f4cc884946e6b2f6c8341fac6dfc1e05
ACPSYan <njhaoyan@gmail.com>
2025-07-14 17:54:42 -0700

    bug in prepareAnnotations.R

--- phenotyping/scripts/txrevise/prepareAnnotations.R
+++ phenotyping/scripts/txrevise/prepareAnnotations.R
@@ -22,15 +22,16 @@ library("GenomicFeatures")
 #Import transcript tags
 transcript_tags = readr::read_tsv(tags_file) %>%
   dplyr::rename(ensembl_transcript_id = transcript_id)
+transcript_meta = txrevise::importTranscriptMetadataFromGTF(gtf_file, transcript_tags)
+
+#Filter the metadata
+#filtered_metadata = txrevise::filterTranscriptMetadata(transcript_meta)
 # Default complete_transcripts is c("protein_coding", "lincRNA"), but Ensembl
 # uses "lncRNA" instead of "lincRNA"
 filtered_metadata = txrevise::filterTranscriptMetadata(
   transcript_meta,
   complete_transcripts = c("protein_coding", "lincRNA", "lncRNA")
 )
-
-#Filter the metadata
-filtered_metadata = txrevise::filterTranscriptMetadata(transcript_meta)
 
 #Construct TxDb
 txdb = GenomicFeatures::makeTxDbFromGFF(gtf_file)
